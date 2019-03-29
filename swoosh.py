2e8521bcc23d0739ba95c37c15e611a7af556cdc
shlomi ziskin ziv <shlomiziskin@gmail.com>
2019-03-29 15:39:09 +0300

    deleted old stuff, added docstrings to get_all_swoosh

--- swoosh.py
+++ swoosh.py
@@ -7,181 +7,6 @@ for the stratosphere from different satallite platforms)
 @author: shlomi
 """
 
-
-#def prepare_swoosh(swoosh_filename='swoosh-v02.6-198401-201802-lonlatpress-20deg-5deg-L31.nc', 
-#                   output_file='swoosh_interpolated.nc'):
-#    from netCDF4 import Dataset
-#    import os, os.path
-#    import numpy as np
-#    import sys
-#    if sys.platform == 'linux':
-#        path = '/home/shlomi/Desktop/DATA/Work Files/Chaim_Stratosphere_Data/'
-#    elif sys.platform == 'darwin':  # mac os
-#        path = '/Users/shlomi/Documents/Chaim_Stratosphere_Data/'
-#    if os.path.isfile(os.path.join(path, output_file)):
-#        print('filename already exists...delete or pick another name')
-#        return
-#    else:
-#        print('filename is good, carrying on...')
-#    sw = get_swoosh_data()
-#    varlist = [name for name in sw.keys() if 'combined' in name.lower()]
-##    for i in range(len(varlist)):
-#        
-#    int_h2o, levels = interp_sw_levels(sw, 'h2o')
-#    int_o3, levels = interp_sw_levels(sw, 'o3')
-#    sw_interpolated = sw.copy()
-#    sw_interpolated['level'] = levels
-#    sw_interpolated['h2o'] = int_h2o
-#    sw_interpolated['o3'] = int_o3
-#    lst = []
-#    # copy all variables from swoosh nc file
-#    with Dataset(path + swoosh_filename) as src, Dataset(path + output_file, "w") as dst:
-#        # copy global attributes all at once via dictionary
-#        dst.setncatts(src.__dict__)
-#        for name, variable in src.variables.items():
-#            lst.append(name)
-#        toexclude = lst[10:]
-#        toexclude.append('level')
-#        # copy attributes
-#        for name in src.ncattrs():
-#            dst.setncattr(name, src.getncattr(name))
-#        # copy dimensions
-#        for name, dimension in src.dimensions.items():
-#            if name not in toexclude:
-#                dst.createDimension(
-#                    name, (len(dimension) if not dimension.isunlimited else None))
-#        # copy all file data except for the excluded
-#        for name, variable in src.variables.items():
-#            if name not in toexclude:
-#                x = dst.createVariable(name, variable.datatype, variable.dimensions)
-#                dst.variables[name][:] = src.variables[name][:]
-#                # copy variable attributes all at once via dictionary
-#                dst[name].setncatts(src[name].__dict__)
-#        level = dst.createDimension('level', len(sw_interpolated['level']))
-#        levels = dst.createVariable('level', np.float32, ('level',))
-#        levels.units = 'hPa'
-#        levels.long_name = 'pressure'
-#        levels.positive = 'down'
-#        levels.axis = 'Z'
-#        h2o = dst.createVariable('h2o', np.float32, ('time', 'level', 'lat', 'lon'))
-#        o3 = dst.createVariable('o3', np.float32, ('time', 'level', 'lat', 'lon'))
-#        h2o.long_name = 'Combined H2O seasonal cycle'
-#        h2o.grid_type = 'geographic'
-#        h2o._Fillvalue = 'nan'
-#        h2o.units = 'ppmv'
-#        o3.long_name = 'Combined O3 seasonal cycle'
-#        o3.grid_type = 'geographic'
-#        o3._Fillvalue = 'nan'
-#        o3.units = 'ppmv'
-#        h2o[:, :, :, :] = sw_interpolated['h2o']
-#        o3[:, :, :, :] = sw_interpolated['o3']
-#        levels[:] = sw_interpolated['level']
-#        dst.description = 'Interpolated level from MERRA GCM levels, data from swoosh file: ' + swoosh_filename 
-#    print('nc file ten to ' + path + output_file)
-#    return # sw_interpolated
-#
-#
-#def interp_sw_levels(swoosh_data, varname='h2o', *levels):
-#    """takes swoosh data and interpolate it vertically to MERRA GCM pressure levels"""
-#    # *levels is other optional pressure levels
-#    from scipy.interpolate import interp1d
-#    from scipy.interpolate import UnivariateSpline
-#    import numpy as np
-#    MERRA_levels = np.array([3.00000000e+02,   2.50000000e+02,   2.00000000e+02,
-#                             1.50000000e+02,   1.00000000e+02,   7.00000000e+01,
-#                             5.00000000e+01,   4.00000000e+01,   3.00000000e+01,
-#                             2.00000000e+01,   1.00000000e+01,   7.00000000e+00,
-#                             5.00000000e+00,   4.00000000e+00,   3.00000000e+00,
-#                             2.00000000e+00,   1.00000000e+00,   7.00000000e-01,
-#                             5.00000000e-01,   4.00000000e-01,   3.00000000e-01,
-#                             1.00000000e-01])
-#    logMlevels = np.flip(np.log(MERRA_levels), 0)
-#    levels = swoosh_data['level']
-#    sw_lon = swoosh_data['lon']
-#    sw_lat = swoosh_data['lat']
-#    loglevels = np.flip(np.log(levels), 0)
-#    var = swoosh_data[varname]
-#    var_out = np.empty((var.shape[0], len(MERRA_levels), var.shape[-2],
-#                        var.shape[-1]), dtype=var.dtype)
-#
-##    def ma_interp(newx, x, y, mask, propagate_mask=True):
-##        newy = np.interp(newx, x[~mask], y[~mask])  # interpolate data
-##        if propagate_mask:  # interpolate mask & apply to interpolated data
-##            newmask = mask[:]
-##            newmask[mask] = 1
-##            newmask[~mask] = 0
-##            newmask = np.interp(newx, x, newmask)
-##            newy = np.ma.masked_array(newy, newmask > 0.5)
-##        return newy
-#
-#    for time in range(var.shape[0]):
-#        print(time)
-#        for lon in range(len(sw_lon)):
-#            for lat in range(len(sw_lat)):
-#                y = np.flip(var[time, :, lat, lon].squeeze(), 0)
-#                # if sum(np.isnan(y)) < 31 - 3:
-#                w = np.isnan(y)
-#                y[w] = 0
-#                # f = interp1d(loglevels, y, kind='linear', assume_sorted=False)
-#                f = UnivariateSpline(loglevels, y, w=~w, ext=1, check_finite=False)
-#                # newy = ma_interp(logMlevels, loglevels, y, y.mask)
-#                var_out[time, :, lat, lon] = np.flip(f(logMlevels), 0)
-#                # swoosh_interpolated[varname][time, :, lat, lon] = newy
-#    var_out[var_out <= 0] = 0
-#    return var_out, MERRA_levels
-#
-#def get_swoosh_data(filename='swoosh-v02.6-198401-201802-lonlatpress-20deg-5deg-L31.nc'):
-#    """Takes files in the path and outputs the combined_h20/o3_q data and the
-#    lat, lon, level etc..."""
-#    import os, os.path
-#    import sys
-#    from netCDF4 import Dataset, num2date
-#    # import netCDF4
-#    import pandas as pd
-#    from mpl_toolkits.basemap import Basemap
-#    import numpy as np
-#    import matplotlib.pyplot as plt
-#    if sys.platform == 'linux':
-#        path = '/home/shlomi/Desktop/DATA/Work Files/Chaim_Stratosphere_Data/'
-#    elif sys.platform == 'darwin':  # mac os
-#        path = '/Users/shlomi/Documents/Chaim_Stratosphere_Data/'
-#    if os.path.isfile(os.path.join(path, filename)):
-#        f = Dataset(path + filename)
-#        print('Importing ' + path + filename)
-#    else:
-#        print('File not found...')
-##    filenames = ([name for name in os.listdir(path) if
-##                  os.path.isfile(os.path.join(path, name))])
-##    f = []
-##    for filename in filenames:
-##        f.append(Dataset(path + filename))
-#    f.set_auto_maskandscale(False)  # disable return of masked arrays where nan is fillvalue
-#    lat = f.variables['lat'][:]
-#    lon = f.variables['lon'][:]
-#    level = f.variables['level'][:]
-#    time = f.variables['time'][:]
-#    latbound = f.variables['latbound'][:]
-#    lonbound = f.variables['lonbound'][:]
-#    month = f.variables['month'][:]
-#    year = f.variables['year'][:]
-#    yrtime = f.variables['yrtime'][:]
-#    jultime = f.variables['jultime'][:]
-#    combinedanomh2oq = f.variables['combinedanomh2oq'][:]
-#    combinedh2oq = f.variables['combinedh2oq'][:]
-#    combinedseash2oq = f.variables['combinedseash2oq'][:]
-#    combinedo3q = f.variables['combinedo3q'][:]
-#    combinedseaso3q = f.variables['combinedseaso3q'][:]
-#    combinedanomo3q = f.variables['combinedanomo3q'][:]
-#    # combined_h2o_q = np.ma.filled(combined_h2o_q.astype(float), np.nan)
-#    # combined_o3_q = np.ma.filled(combined_o3_q.astype(float), np.nan)
-#    swoosh = {'lat': lat, 'lon': lon, 'level': level, 'time': time,
-#              'latbound': latbound, 'lonbound': lonbound, 'month': month,
-#              'year': year, 'yrtime': yrtime, 'jultime': jultime,
-#              'combinedh2oq': combinedh2oq, 'combinedanomh2oq': combinedanomh2oq,
-#              'combinedseash2oq': combinedseash2oq, 'combinedo3q': combinedo3q,
-#              'combinedseaso3q': combinedseaso3q, 'combinedanomo3q': combinedanomo3q}
-#    return swoosh
-
 def get_swoosh_xarray(filename='swoosh-v02.6-198401-201802-latpress-5deg-L31.nc'):
     """Takes files in the path and outputs the combined_h20/o3_q data and the
     lat, lon, level etc..."""
@@ -316,6 +141,9 @@ def get_all_swoosh_files(path):
     import xarray as xr
     import fnmatch
     import pandas as pd
+    """Reads all swoosh dataset downloaded from swoosh website and write
+    them as xarray netcdf files, basically adding datetime index and importing
+    just the 'combined' fields."""
     filenames = []
     for file in os.listdir(path + '.'):
         if fnmatch.fnmatch(file, 'swoosh-v02.6*.*'):
@@ -325,15 +153,16 @@ def get_all_swoosh_files(path):
         dataset = xr.open_dataset(path + sw_filename, decode_times=False)
         dataset.attrs['filename'] = sw_filename
         datasets.append(dataset)
-        print ('importing ' + sw_filename)
+        print('importing ' + sw_filename)
     data_dict = {}
     for i in range(len(datasets)):
         k = list(datasets[i].data_vars.keys())
-        combined_list = [name for name in datasets[i].data_vars.keys() 
+        combined_list = [name for name in datasets[i].data_vars.keys()
                          if 'combined' in name.lower()]
         to_drop = list(set(k) - set(combined_list))
         datasets[i] = datasets[i].drop(to_drop)
-        time = pd.date_range('1984-01-01', freq='MS', periods=len(datasets[i].time))
+        time = pd.date_range('1984-01-01', freq='MS',
+                             periods=len(datasets[i].time))
         datasets[i]['time'] = time
         if 'latpress' in datasets[i].attrs['filename'].split('-'):
             dname = '-'.join(datasets[i].attrs['filename'].split('-')[-3:-1])
@@ -344,10 +173,6 @@ def get_all_swoosh_files(path):
         data_dict[dname] = datasets[i]
         data_dict[dname].to_netcdf(path + 'swoosh_' + dname + '.nc')
         print('Saved ' + dname + ' to nc file, in ' + path)
-    # remove isentropic levels datasets:
-#    del datasets[0]
-#    del datasets[2]
-#    del datasets[5]
     return data_dict
 
 
