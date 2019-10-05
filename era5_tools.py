a7e4a1235c3406c894c8190760d6b8bdbf65460e
shlomi <shlomiziskin@gmail.com>
2019-10-05 18:18:00 +0300

    added model_level to pressure levels ERA5 functions

--- era5_tools.py
+++ era5_tools.py
@@ -7,6 +7,151 @@ Created on Fri Nov 23 12:06:16 2018
 """
 
 
+#def transform_l137_ml_to_pressure(ds, l137_path):
+#    """transform model levels from int numbers to hPa full pressure levels"""
+#    l137 = read_L137_to_ds(l137_path)
+#    levels = ds.level.values
+#    pf = l137.sel(n=levels).pf.values
+#    ds['level'] = pf
+#    return ds
+#def ps_ufunc(x, a, b, n):
+#    import xarray as xr
+#    out = xr.apply_ufunc(pressure_from_ab, x, a, b, n, join='outer', output_core_dims=['n', 'time', 'latitude', 'longitude'])
+#    return out
+
+
+def transform_model_levels_to_pressure(path, field_da, plevel=85.0, mm=True):
+    """takes a field_da (like t, u) that uses era5 L137 model levels and
+    interpolates it using pf(only monthly means) to desired plevel"""
+    # TODO: do scipy.interpolate instead of np.interp (linear)
+    # TODO: add multiple plevel support
+    import xarray as xr
+    from aux_functions_strat import dim_intersection
+    import numpy as np
+    if mm:
+        pf = xr.open_dataset(path / 'era5_full_pressure_mm_1979-2018.nc')
+    pf = pf.pf
+    levels = dim_intersection([pf, field_da], dropna=False, dim='level')
+    pf = pf.sel(level=levels)
+    field_da = field_da.sel(level=levels)
+    field_da = field_da.transpose('time', 'latitude', 'longitude', 'level')
+    field = field_da.values
+    da = np.zeros((pf.time.size, pf.latitude.size, pf.longitude.size, 1),
+                  'float32')
+    pf = pf.values
+    for t in range(pf.shape[0]):
+        print(t)
+        for lat in range(pf.shape[1]):
+            for lon in range(pf.shape[2]):
+                pressures = pf[t, lat, lon, :]
+                vals = field[t, lat, lon, :]
+                da[t, lat, lon, 0] = np.interp(plevel, pressures, vals)
+    da = xr.DataArray(da, dims=['time', 'lat', 'lon', 'level'])
+    da['level'] = [plevel]
+    da['time'] = field_da['time']
+    da['level'].attrs['long_name'] = 'pressure_level'
+    da['level'].attrs['units'] = 'hPa'
+    da['lat'] = field_da['latitude'].values
+    da['lat'].attrs = field_da['latitude'].attrs
+    da['lon'] = field_da['longitude'].values
+    da['lon'].attrs = field_da['longitude'].attrs
+    da.name = field_da.name
+    da.attrs = field_da.attrs
+    da = da.sortby('lat')
+    comp = dict(zlib=True, complevel=9)  # best compression
+    encoding = {var: comp for var in da.to_dataset(name=da.name).data_vars}
+    filename = 'era5_' + da.name + '_' + str(int(plevel)) + 'hPa.nc'
+    da.to_netcdf(path / filename, encoding=encoding)
+    return da
+
+
+def create_model_levels_map_from_surface_pressure(work_path):
+    import numpy as np
+    import xarray as xr
+    """takes ~24 mins for monthly mean with 1.25x1.25 degree for full 137
+    levels,~=2.9GB file. Don't try with higher sample rate"""
+    ds = read_L137_to_ds(work_path)
+    a = ds.a.values
+    b = ds.b.values
+    n = ds.n.values
+    sp = xr.open_dataset(work_path / 'era5_SP_mm_1979-2018.nc')
+    sp = sp.sp / 100.0
+    pf = np.zeros((sp.time.size, sp.latitude.size, sp.longitude.size, len(a)),
+                  'float32')
+    sp_np = sp.values
+    for t in range(sp.time.size):
+        print(t)
+        for lat in range(sp.latitude.size):
+            for lon in range(sp.longitude.size):
+                ps = sp_np[t, lat, lon]
+                pf[t, lat, lon, :] = pressure_from_ab(ps, a, b, n)
+    pf_da = xr.DataArray(pf, dims=['time', 'latitude', 'longitude', 'level'])
+    pf_da.attrs['units'] = 'hPa'
+    pf_da.attrs['long_name'] = 'full_pressure_level'
+    pf_ds = pf_da.to_dataset(name='pf')
+    pf_ds['level'] = n
+    pf_ds['level'].attrs['long_name'] = 'model_level_number'
+    pf_ds['latitude'] = sp.latitude
+    pf_ds['latitude'].attrs = sp.latitude.attrs
+    pf_ds['longitude'] = sp.longitude
+    pf_ds['longitude'].attrs = sp.longitude.attrs
+    pf_ds['time'] = sp.time
+    comp = dict(zlib=True, complevel=9)  # best compression
+    encoding = {var: comp for var in pf_ds.data_vars}
+    pf_ds.to_netcdf(work_path / 'era5_full_pressure_mm_1979-2018.nc',
+                    encoding=encoding)
+    return pf_ds
+
+
+def pressure_from_ab(ps, a, b, n):
+    import numpy as np
+    import xarray as xr
+    ph_shape = a.shape
+    ph = np.empty(ph_shape)
+    pf = np.empty(len(a) - 1)
+    pf_final = np.empty(ph_shape)
+    ph = a / 100.0 + b * ps
+    for i in n[0:-1]:
+        pf[i] = 0.5 * (ph[i] + ph[i + 1])
+    pf_final[0] = np.nan
+    pf_final[1:] = pf
+    # pf_da = xr.DataArray(pf_final, dims='n')
+    # pf_da['n'] = n
+    # pf_da.attrs['units'] = 'hPa'
+    # pf_da['n'].attrs['long_name'] = 'model_level_number'
+    return pf_final  # pf_da
+
+
+def read_L137_to_ds(path):
+    import pandas as pd
+    l137_df = pd.read_csv(path / 'L137_model_levels_1976_climate.txt',
+                          header=None, delim_whitespace=True, na_values='-')
+    l137_df.columns = ['n', 'a', 'b', 'ph', 'pf', 'Geopotential Altitude',
+                       'Geometric Altitude', 'Temperature', 'Density']
+    l137_df.set_index('n')
+    l137_df.drop('n', axis=1, inplace=True)
+    l137_df.index.name = 'n'
+    ds = l137_df.to_xarray()
+    ds.attrs['long_name'] = 'L137 model levels and 1976 ICAO standard atmosphere 1976'
+    ds.attrs['surface_pressure'] = 1013.250
+    ds.attrs['pressure_units'] = 'hPa'
+    ds.attrs['half_pressure_formula'] = 'ph(k+1/2) = a(k+1/2) + ps*b(k+1/2)'
+    ds.attrs['full_pressure_formula'] = 'pf(k) = 1/2*(ph(k-1/2) + ph(k+1/2))'
+    ds['n'].attrs['long_name'] = 'model_level_number'
+    ds['a'].attrs['long_name'] = 'a_coefficient'
+    ds['a'].attrs['units'] = 'Pa'
+    ds['b'].attrs['long_name'] = 'b_coefficient'
+    ds['ph'].attrs['long_name'] = 'half_pressure_level'
+    ds['ph'].attrs['units'] = 'hPa'
+    ds['pf'].attrs['long_name'] = 'full_pressure_level'
+    ds['pf'].attrs['units'] = 'hPa'
+    ds['Geopotential Altitude'].attrs['units'] = 'm'
+    ds['Geometric Altitude'].attrs['units'] = 'm'
+    ds['Temperature'].attrs['units'] = 'K'
+    ds['Density'].attrs['units'] = 'kg/m^3'
+    return ds
+
+
 def compare_MERRA_ERA5(path='local', plot=False):
     import matplotlib.pyplot as plt
     from time_inds_for_MLR import get_BDC, get_T500
